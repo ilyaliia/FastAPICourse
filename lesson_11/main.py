9a5a155d5162bd15364c85433d872c2b9bbfbc67
ilyaliia <ilyaliia@outlook.com>
2026-02-05 01:17:21 +0300

    lesson 11. Depends

---
+++ lesson_11/main.py
@@ -0,0 +1,96 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import select
+
+from fastapi import FastAPI, Depends
+from pydantic import BaseModel, Field
+
+import uvicorn
+
+from typing import Annotated
+
+app = FastAPI()
+
+engine = create_async_engine("sqlite+aiosqlite:///books.db")
+
+new_session = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def get_session():
+    async with new_session() as session:
+        yield session
+
+
+SessionDep = Annotated[AsyncSession, Depends(get_session)]
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class BookModel(Base):
+    __tablename__ = "books"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str]
+    author: Mapped[str]
+
+
+@app.post("/setup_database")
+async def setup_database():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+    return {"success": True}
+
+
+class BookCreateSchema(BaseModel):
+    title: str
+    author: str
+
+
+class BookSchema(BookCreateSchema):
+    id: int
+
+
+@app.post("/books")
+async def add_book(data: BookSchema, session: SessionDep):
+    new_book = BookModel(
+        title=data.title,
+        author=data.author
+
+    )
+    session.add(new_book)
+    await session.commit()
+    return {"success": True}
+
+
+# @app.get("/books")
+# async def get_books(session: SessionDep):
+#     query = select(BookModel)
+#     result = await session.execute(query)
+#     return result.scalars().all()
+
+class PaginationParams(BaseModel):
+    limit: int = Field(5, ge=0, le=100, description="Количество элементов на странице")
+    offset: int = Field(0, ge=0, description="Смещение для пагинации")
+
+
+PaginationDep = Annotated[PaginationParams, Depends(PaginationParams)]
+
+
+@app.get("/books")
+async def get_books(
+        session: SessionDep,
+        pagination: PaginationDep,
+) -> list[BookSchema]:
+    query = (select(BookModel)
+             .limit(pagination.limit)
+             .offset(pagination.offset)
+             )
+    result = await session.execute(query)
+    return result.scalars().all()
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
